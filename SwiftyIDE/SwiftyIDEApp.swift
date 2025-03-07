4622142c21dfd60e0fd012a37f4d0ce3b514cf4c
Serik Mussayev <mussaser@fit.cvut.cz>
2025-03-07 22:00:23 +0100

    Initial Commit

---
+++ SwiftyIDE/SwiftyIDEApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyIDEApp.swift
+//  SwiftyIDE
+//
+//  Created by Serik Musaev on 07.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyIDEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
